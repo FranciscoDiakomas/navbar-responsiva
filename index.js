60b89d0f4b284ba1629ff0080f8b239e32e2de91
Francisco Diakomas <fdiakomas@gmial.com>
2024-04-30 04:38:52 -0300

    add arq

---
+++ index.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+window.onload = function () {
+
+        const btn = document.getElementsByClassName("mg")[ 0 ]
+        btn.addEventListener('click', () => {
+                const mobilebar = document.getElementById('nav-mobile')
+                mobilebar.classList.toggle('flex')
+                const img = document.getElementsByName('img')[0]
+               img.src = '/icons/cross.png'
+               
+        })     
+}
