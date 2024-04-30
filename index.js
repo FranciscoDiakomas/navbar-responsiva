aace36dea3710827ff98a53cfce28766a4319d9a
Francisco Diakomas <fdiakomas@gmial.com>
2024-04-30 12:50:31 -0300

    terminei

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ window.onload = function () {
         const btn = document.getElementsByClassName("mg")[ 0 ]
         btn.addEventListener('click', () => {
                 const mobilebar = document.getElementById('nav-mobile')
-                mobilebar.classList.toggle('flex')
-                const img = document.getElementsByName('img')[0]
-               img.setAttribute('src','/icons/cross.png') 
+                mobilebar.style.display = 'flex'
+                const links = document.querySelectorAll('.nav-li')
+                const img = document.getElementsByTagName('img')[ 0 ]
+                img.setAttribute('src','/icons/cross.png')
+                links.forEach((item) => {
+                        item.addEventListener("click", () => {
+                                const mobilebar = document.getElementById('nav-mobile')
+                                mobilebar.style.display = 'none'
+                                 const img = document.getElementsByTagName('img')[ 0 ]
+                                img.setAttribute('src','/icons/menu-burger.png')
+                        })
+                })
                
-        })      
+        })
+        const sections = document.querySelectorAll('section')
+        sections.forEach((e) => {
+                e.addEventListener('mouseenter', () => {
+                        
+                        const mobilebar = document.getElementById('nav-mobile')
+                        mobilebar.style.display = 'none'
+                        const img = document.getElementsByTagName('img')[ 0 ]
+                        img.setAttribute('src','/icons/menu-burger.png')
+                })
+                
+        })
 }
